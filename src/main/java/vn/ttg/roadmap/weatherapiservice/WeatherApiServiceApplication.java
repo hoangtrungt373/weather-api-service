98ec9cdc45e29a456d49afe8f156b8259b96c243
ttg <ttg@elca.vn>
2025-09-08 19:03:32 +0700

    init

---
+++ src/main/java/vn/ttg/roadmap/weatherapiservice/WeatherApiServiceApplication.java
@@ -0,0 +1,13 @@
+package vn.ttg.roadmap.weatherapiservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherApiServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherApiServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/vn/ttg/roadmap/weatherapiservice/WeatherApiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.ttg.roadmap.weatherapiservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherApiServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
